5add65a0bf1f2ac57c8cdfb96e0d3bc9ddc04c54
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2020-03-01 17:02:51 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title:Text('Hello world!')
+          ),
+          body: Text('Hello world!',
+            style: TextStyle(fontSize: 30),
+          )),
+    );
+  }
+}
+
+class HelloPage extends StatefulWidget {
+
+  final String title;
+
+  HelloPage(this.title);
+
+  @override
+  _HelloPageState createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(widget.title, style: TextStyle(fontSize: 30));
+  }
+}
+
